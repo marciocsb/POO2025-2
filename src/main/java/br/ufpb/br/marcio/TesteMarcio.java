1496392d405675aa092f0748f495461d74dcb709
marcio.cruz <marcio.cruz@dcx.ufpb.br>
2025-11-11 09:31:08 -0300

    Criando nova classe de teste usando IO

---
+++ src/main/java/br/ufpb/br/marcio/TesteMarcio.java
@@ -0,0 +1,10 @@
+package br.ufpb.br.marcio;
+
+public class TesteMarcio {
+    void main(){
+        IO.print("Oi");
+        String nome = IO.readln("Digite seu nome:");
+        IO.println(" Oi" + nome);
+
+    }
+}
