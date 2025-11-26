a6ae3865ba9cffcc4789a8271b77034e1436da16
marciocsb <marcio.cruz@dcx.ufpb.br>
2025-11-26 14:50:08 -0300

    Delete AmigoInexistenteException.java

--- AmigoInexistenteException.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.dxc.marcioAmigoSecreto;
-
-public class AmigoInexistenteException extends RuntimeException {
-    public AmigoInexistenteException(String message) {
-        super(message);
-    }
-}
