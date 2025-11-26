1ee757e9f6320eb10b3e2d2e1fac15fb88b0ef5a
marciocsb <marcio.cruz@dcx.ufpb.br>
2025-11-26 14:49:05 -0300

    Delete AmigoSorteadoException.java

--- AmigoSorteadoException.java
+++
@@ -1,7 +0,0 @@
-package br.ufpb.dxc.marcioAmigoSecreto;
-
-public class AmigoSorteadoException extends RuntimeException {
-    public AmigoSorteadoException(String message) {
-        super(message);
-    }
-}
