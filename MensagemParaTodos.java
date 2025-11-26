876a2f84f3535b886f08831eb4d333d4c85c75c0
marciocsb <marcio.cruz@dcx.ufpb.br>
2025-11-26 14:50:54 -0300

    Delete MensagemParaTodos.java

--- MensagemParaTodos.java
+++
@@ -1,9 +0,0 @@
-package br.ufpb.dcx.amigosecreto;
-
-public class MensagemParaTodos extends Mensagem {
-
-    public MensagemParaTodos(String texto, String emailRemetente, boolean anonimo) {
-        super(texto, emailRemetente, anonimo);
-
-    }
-}
